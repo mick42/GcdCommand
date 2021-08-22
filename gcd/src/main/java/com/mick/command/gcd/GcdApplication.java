691f644e330c79d5c0a94ad743372ebb8b15f124
Mick <mick@localhost.tbo>
2021-08-22 14:49:57 +0100

    Initial commit of spring starter

---
+++ gcd/src/main/java/com/mick/command/gcd/GcdApplication.java
@@ -0,0 +1,13 @@
+package com.mick.command.gcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcdApplication.class, args);
+	}
+
+}
